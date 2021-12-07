ee4f99a8c5a1984195652bc96abee0928428eede
Sumeet Kumar <mailtosumeet88@gmail.com>
2021-12-06 21:25:36 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,55 @@
-package com.company;
 
 public class Main {
+
+    /**
+     *  It transforms stream of characters into a every alternate character as lower to upper
+     *  ( It assumes empty character as also character stream .)
+     *  Time Complexity O(n/2)
+     * @param word
+     * @return
+     */
+    public static String tranform(String word){
+
+        int len = word.length();
+        StringBuffer accumulator = new StringBuffer();
+
+        for (int i = 0; i <len ; i++) {
+            char c =  word.charAt(i);
+            if(i%2 == 0 && c!='i'){
+                accumulator.append(Character.toUpperCase(c));
+            }else {
+                accumulator.append(Character.toLowerCase(c));
+            }
+        }
+        return  accumulator.toString();
+    }
+
+    /**
+     *  It transforms stream of characters into a every alternate character as lower to upper
+     *  ( It assumes empty character as also character stream .)
+     *  Time Complexity O(n/2)
+     * @param word
+     * @return
+     */
+    public static String transformFast(String word){
+        int len = word.length();
+        char[] accumulator = new char[len];
+
+        for (int i = 0,j=len-1; i <= j; i++,j--) {
+            char left =  word.charAt(i);
+            char right = word.charAt(j);
+            accumulator[i]= Character.toLowerCase(left);
+            accumulator[j]= Character.toLowerCase(right);
+            if( (left != 'i' || left != 'I') && i%2 == 0){
+                accumulator[i]= Character.toUpperCase(left);
+            }
+            if((right != 'i'|| right != 'I') && j%2 == 0){
+                accumulator[j]= Character.toUpperCase(right);
+            }
+        }
+        return String.valueOf(accumulator);
+    }
+
     public static void main(String[] args) {
         String input = "This is cisco interview";
 
@@ -39,52 +88,4 @@ public class Main {
         System.out.println(outputFast);
         assert outputFast == "AbCd";
     }
-
-    /**
-     *  It transforms stream of characters into a every alternate character as lower to upper
-     *  ( It assumes empty character as also character stream .)
-     * @param word
-     * @return
-     */
-    public static String tranform(String word){
-
-        int len = word.length();
-        StringBuffer accumulator = new StringBuffer();
-
-        for (int i = 0; i <len ; i++) {
-            char c =  word.charAt(i);
-            if(i%2 == 0 && c!='i'){
-                accumulator.append(Character.toUpperCase(c));
-            }else {
-                accumulator.append(Character.toLowerCase(c));
-            }
-        }
-        return  accumulator.toString();
-    }
-
-    /**
-     *  It transforms stream of characters into a every alternate character as lower to upper
-     *  ( It assumes empty character as also character stream .)
-     * @param word
-     * @return
-     */
-    public static String transformFast(String word){
-        int len = word.length();
-        char[] accumulator = new char[len];
-
-        for (int i = 0,j=len-1; i <= j; i++,j--) {
-            char left =  word.charAt(i);
-            char right = word.charAt(j);
-            accumulator[i]= Character.toLowerCase(left);
-            accumulator[j]= Character.toLowerCase(right);
-            if(left != 'i' && i%2 == 0){
-                accumulator[i]= Character.toUpperCase(left);
-            }
-            if(right != 'i' && j%2 == 0){
-                accumulator[j]= Character.toUpperCase(right);
-            }
-        }
-        return String.valueOf(accumulator);
-    }
-
 }
